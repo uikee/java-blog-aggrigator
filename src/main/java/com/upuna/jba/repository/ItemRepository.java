dc31fd296ede7aa75bcc87583a8213cc6123fa8b
Upuna Iresha <upunairesha@gmail.com>
2017-06-08 11:45:42 +0530

    paginations

--- src/main/java/com/upuna/jba/repository/ItemRepository.java
+++ src/main/java/com/upuna/jba/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package com.upuna.jba.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.upuna.jba.entity.Blog;
@@ -9,6 +10,6 @@ import com.upuna.jba.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 	
-	List<Item> findByBlog(Blog blog);
+	List<Item> findByBlog(Blog blog, Pageable pageable);
 	
 }
